55c3c05060e2a5e4ef11cf3461f64836a64b21a6
Sherief Farid <sherief@farid.cloud>
2019-03-16 16:39:40 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -1,4 +1,4 @@
-
+require 'pry'
 def hopper
 	programmer_hash =
  		{
@@ -15,7 +15,8 @@ def hopper
           :languages => ["C"]
         }
      }
-return [programmer_hash][grace_hopper]
+		 binding.pry
+return [programmer_hash][:grace_hopper]
 
 end
 
